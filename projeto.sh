cfd9986f8f2849ae1db4d2523ff256f5c183e26c
Matheus <martinssmatheus@hotmail.com>
2022-12-13 15:07:18 -0300

    altearção script

--- projeto.sh
+++ projeto.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 ## deploy volumes
+echo VOLUMES
 kubectl apply -f volumes.yml
 
 ## deploy deployments
+echo DEPLOYMENTS
 kubectl apply -f deployment.yml
 
 ## deploy services
+echo SERVICES
 kubectl apply -f services.yml
 
 echo deploy projeto ok!!
